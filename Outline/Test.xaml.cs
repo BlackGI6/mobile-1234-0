fd324bfddff8ab7bfd0dfea73592599f25c00c3d
Vlada Misici <33636824+BlackGI6@users.noreply.github.com>
2021-03-28 01:50:36 +0200

    Created Draw polyline method between 2 points

--- Outline/Test.xaml.cs
+++ Outline/Test.xaml.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
+using Xamarin.Essentials;
+using System.Diagnostics;
 
 namespace Outline
 {
@@ -15,6 +18,43 @@ namespace Outline
         public Test()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Geocoder geocoder = new Geocoder();
+
+                IEnumerable<Position> approximateLocations = await geocoder.GetPositionsForAddressAsync(Entry.Text);
+                Position position = approximateLocations.FirstOrDefault();
+                Pin pin = new Pin
+                {
+                    Label = "Test label sa vad cum arata lol tare",
+                    Address = Entry.Text,
+                    Type = PinType.Place,
+                    Position = position
+                };
+                map.Pins.Add(pin);
+                //Create polyline between current position and pin
+
+                //get current position
+                var currentLocation = await Geolocation.GetLastKnownLocationAsync();
+                string currentlocation_lat = currentLocation.Latitude.ToString();
+                string currentlocation_lng = currentLocation.Longitude.ToString();
+                string pinlocation_lat = position.Latitude.ToString();
+                string pinlocation_lng = position.Longitude.ToString();
+                if (currentLocation != null)
+                {
+                    Polyline_Visual.DrawPolyline drawPolyline = new Polyline_Visual.DrawPolyline(currentlocation_lat, currentlocation_lng, pinlocation_lat, pinlocation_lng);
+                    map.MapElements.Add(drawPolyline.polyline);
+                }
+            }
+            catch(Exception er)
+            {
+                Trace.WriteLine(er.Message);
+                Trace.WriteLine("error from ButtonClicked(test.xaml.cs) @" + DateTime.Now);
+            }
         }
     }
 }
