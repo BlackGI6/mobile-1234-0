89179c2d7022ed9892b9b96a53be6bab37ebf823
CristiStratulat <43218916+CristiStratulat@users.noreply.github.com>
2021-03-28 02:04:54 +0200

    Register Working

--- Outline/Views/Forms/SimpleSignUpPage.xaml.cs
+++ Outline/Views/Forms/SimpleSignUpPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms.Internals;
+﻿using Outline.Helper;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace Outline.Views.Forms
@@ -13,9 +15,37 @@ namespace Outline.Views.Forms
         /// <summary>
         /// Initializes a new instance of the <see cref="SimpleSignUpPage" /> class.
         /// </summary>
+         IFirebaseRegister auth;
+        readonly FirebasePointHelper firebaseClient = new FirebasePointHelper();
         public SimpleSignUpPage()
         {
             InitializeComponent();
+            auth = DependencyService.Get<IFirebaseRegister>();
         }
+
+        private void SfButton_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private async void SfButton_Clicked_1(object sender, System.EventArgs e)
+        {
+            string Token = "";
+            bool connection = true;
+            try
+            {
+                Token = await auth.RegisterWithEmailAndPassword(EmailEntry.Text,PasswordEntry.Text);
+            }
+            catch
+            {
+                connection = false;
+            }
+            if(connection)
+            {
+               await firebaseClient.AddNgo(App.UserUID,Address.Text);
+                await Navigation.PushAsync(new SimpleLoginPage());
+            }
+        }
+        
     }
 }
